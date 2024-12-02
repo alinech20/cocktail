3d26ebceaa90ce43fb5262e4ba1930c1f65754a8
Alin Nechita <alin.nechita20@gmail.com>
2024-12-02 14:04:15 +0200

    Add health endpoint

--- backend/v1/api.py
+++ backend/v1/api.py
@@ -5,3 +5,7 @@ from .endpoints import ingredients, recipes
 router = APIRouter(prefix="/api/v1")
 router.include_router(ingredients.router)
 router.include_router(recipes.router)
+
+@router.get("/health")
+def hello():
+  return { "message": "Hello world!" }
